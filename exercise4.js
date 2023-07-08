d42422073987c6585deca0693803bbe52c37ab91
unknown <danielaxaavier@hotmail.comgit config --global user.name DanielaFehlowgit config --global user.email danielaxaavier@hotmail.com>
2023-07-08 10:38:16 -0300

    Third Commit

--- exercise4.js
+++ exercise4.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function firstLetterUpperCase(input) {
   const sentences = input.split(/([.!?]+)/)
   for (let i = 0; i < sentences.length; i += 2) {
@@ -14,20 +13,3 @@ function firstLetterUpperCase(input) {
 let input = "hello. how are you? i'm fine, thank you."
 const output = firstLetterUpperCase(input)
 console.log(output)
-=======
-function firstLetterUpperCase(input) {
-  const sentences = input.split(/([.!?]+)/)
-  for (let i = 0; i < sentences.length; i += 2) {
-    const sentenceBlank = sentences[i].trim()
-    if (sentenceBlank.length > 0) {
-      sentences[i] =
-        sentenceBlank.charAt(0).toUpperCase() + sentenceBlank.slice(1)
-    }
-  }
-  return sentences.join('')
-}
-
-let input = "hello. how are you? i'm fine, thank you."
-const output = firstLetterUpperCase(input)
-console.log(output)
->>>>>>> 6a5de07660262dfaed24a2f227dd75e4c9b768cb
